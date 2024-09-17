5f6a395ed2ac3b11d22f9941ae8d59ba2d11fed9
Shuudy <ancieux.jerome059@gmail.com>
2024-09-17 14:02:16 +0200

    Add links in sidebar with his scss style

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,3 +1,9 @@
 <div class="sidebar">
     <div class="sidebar-logo">Echo Pulse</div>
+    <div class="sidebar-sep"></div>
+    <div class="sidebar-links">
+        @for($i=0;$i<15;$i++)
+        <a class="sidebar-link" href="#">Test</a>
+        @endfor
+    </div>
 </div>
