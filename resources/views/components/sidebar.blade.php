f82a33d85c5187b8cb7ae2cfd9451290eb67ef0d
Shuudy <ancieux.jerome059@gmail.com>
2024-09-18 12:03:23 +0200

    Add music info with his photo in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,7 +3,15 @@
     <div class="sidebar-sep"></div>
     <div class="sidebar-links">
         @for($i=0;$i<15;$i++)
-        <a class="sidebar-link" href="#">Test</a>
+        <a class="sidebar-link" href="#">
+            <div class="sidebar-music-photo">
+                <img src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="" srcset="">
+            </div>
+            <div class="sidebar-music-info">
+                <span>Test</span>
+                <span>Test</span>
+            </div>
+        </a>
         @endfor
     </div>
 </div>
