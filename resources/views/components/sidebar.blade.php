bb8a88b202abcf9c6d56ea53850d733986c573b6
O6nHdel <hugo.delaporte@gmail.com>
2024-09-18 10:13:26 +0200

    add show musique in sidebar and create musique routes

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,8 +2,8 @@
     <div class="sidebar-logo">Echo Pulse</div>
     <div class="sidebar-sep"></div>
     <div class="sidebar-links">
-        @for($i=0;$i<15;$i++)
-        <a class="sidebar-link" href="#">Test</a>
-        @endfor
+        @foreach($musiques as $musique)
+        <a class="sidebar-link" href="{{ route("musique.show", $musique['id']) }}">{{ $musique['titre'] }}</a>
+        @endforeach
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\MusiqueController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::view('/layout', 'layout');
+
+Route::get('/musique',[MusiqueController::class,'index']);
+
+Route::get('/musique/{musique}',[MusiqueController::class,'show'])->name("musique.show");
