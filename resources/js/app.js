9ab13a45f368a65c15288e3e817bd94819a2ba83
Shuudy <ancieux.jerome059@gmail.com>
2024-09-18 11:20:18 +0200

    Edit mediabar button and add javascript logic

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,55 @@
 import '../css/app.scss';
+
+const audioElement = document.querySelector("audio");
+const audioCtx = new AudioContext();
+const track = audioCtx.createMediaElementSource(audioElement);
+
+const progressBarContainer = document.querySelector(".mediabar-progressbar-container");
+const progressBarFilled = document.querySelector(".mediabar-progressbar");
+
+const playButton = document.querySelector(".mediabar-playpause-playbutton");
+const playIcon = document.querySelector(".mediabar-playpause-playicon");
+
+const pauseIcon = document.querySelector(".mediabar-playpause-pauseicon");
+
+audioElement.addEventListener("timeupdate", () => {
+    progressUpdate();
+    console.log("timeupdate");
+});
+
+playButton.addEventListener('click', () => {
+    console.log("Click play button")
+    // Si l'audio est en stop, alors on démarre l'audio
+    if (audioCtx.state === "suspended") {
+        audioCtx.resume();
+    }
+
+    if (playButton.dataset.playing === "false") {
+        audioElement.play();
+
+        playButton.dataset.playing = "true";
+        playIcon.classList.add("mediabar-hidden");
+        pauseIcon.classList.remove("mediabar-hidden");
+    } else if (playButton.dataset.playing === "true") {
+        audioElement.pause();
+
+        playButton.dataset.playing = "false";
+        pauseIcon.classList.add("mediabar-hidden");
+        playIcon.classList.remove("mediabar-hidden");
+    }
+});
+
+const gainNode = audioCtx.createGain();
+
+const volumeControl = document.querySelector(".volume-control");
+    volumeControl.addEventListener("input", () => {
+        gainNode.gain.value = volumeControl.value;
+    });
+
+track.connect(gainNode).connect(audioCtx.destination);
+
+function progressUpdate() {
+    const percent = (audioElement.currentTime / audioElement.duration) * 100;
+    progressBarFilled.style.width = `${percent}%`;
+}
+
--- resources/views/components/mediabar.blade.php
+++ resources/views/components/mediabar.blade.php
@@ -15,15 +15,17 @@
                 </svg>
             </div>
             <div class="mediabar-playpause">
-                <svg class="mediabar-playpause-playicon" xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" color="#fff" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="currentColor" />
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" class="mediabar-playpause-pauseicon mediabar-hidden" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" color="#fff" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
-                    <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
-                </svg>
+                <div class="mediabar-playpause-playbutton" data-playing="false">
+                    <svg class="mediabar-playpause-playicon" xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" color="#fff" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="currentColor" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mediabar-playpause-pauseicon mediabar-hidden" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" color="#fff" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
+                        <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
+                    </svg>
+                </div>
             </div>
             <div class="mediabar-after">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" color="#fff" stroke-linecap="round" stroke-linejoin="round">
@@ -42,7 +44,7 @@
                 <path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" />
             </svg>
 
-            <input type="range" id="volume-control" min="0" max="1" step="0.01" value="1">            
+            <input type="range" class="volume-control" min="0" max="1" step="0.01" value="1">            
         </div>
     </div>
 </div>
