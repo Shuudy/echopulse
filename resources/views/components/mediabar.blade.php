bc5ea3262642e19c33bd25c32a076fc41fe79064
Shuudy <ancieux.jerome059@gmail.com>
2024-09-18 09:57:03 +0200

    Add play and pause icon with mediabar-hidden class

--- resources/views/components/mediabar.blade.php
+++ resources/views/components/mediabar.blade.php
@@ -15,9 +15,14 @@
                 </svg>
             </div>
             <div class="mediabar-playpause">
-                <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" color="#fff" stroke-linecap="round" stroke-linejoin="round">
+                <svg class="mediabar-playpause-playicon" xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" color="#fff" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="currentColor" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="mediabar-playpause-pauseicon mediabar-hidden" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" color="#fff" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
+                    <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
                 </svg>
             </div>
             <div class="mediabar-after">
