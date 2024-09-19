b8ea3c178ab4a21989d857f893b78b17a8a4b249
O6nHdel <hugo.delaporte@gmail.com>
2024-09-19 11:19:12 +0200

    changing jpg to webp

--- database/seeders/MusiqueSeeder.php
+++ database/seeders/MusiqueSeeder.php
@@ -15,7 +15,7 @@ class MusiqueSeeder extends Seeder
     {
         $musiques = [
             ['titre' => 'apdl', 'nom_artiste'=> 'Alpha wann', 'album' => 'Don dada maxtape', 'date_sortie' => '2024-09-19', 'chemin_fichier_audio' => 'musique3.mp3', 'chemin_fichier_image' => 'musique4.jpg'],
-            ['titre' => 'Mami Wata', 'nom_artiste'=> 'Gazo', 'album' => 'La Melo est gangx', 'date_sortie' => '2024-09-18', 'chemin_fichier_audio' => 'musique1.mp3', 'chemin_fichier_image' => 'musique2.jpg'],
+            ['titre' => 'Mami Wata', 'nom_artiste'=> 'Gazo', 'album' => 'La Melo est gangx', 'date_sortie' => '2024-09-18', 'chemin_fichier_audio' => 'musique1.mp3', 'chemin_fichier_image' => 'musique2.webp'],
             ['titre' => 'Nouvelles', 'nom_artiste'=> 'PLK', 'album' => '2069', 'date_sortie' => '2024-09-18', 'chemin_fichier_audio' => 'musique2.mp3', 'chemin_fichier_image' => 'musique3.jpg']
         ];
         Musique::insert($musiques);
