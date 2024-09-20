da192075e44c3a3780c19d6e59dfc44560ba3e3f
O6nHdel <hugo.delaporte@gmail.com>
2024-09-20 12:21:41 +0200

    adding title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{ $musique['titre'] }} - Echo Pulse</title>
     @vite(['resources/js/app.js'])
 </head>
 <body>
