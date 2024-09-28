8ad6787b6fb1f0fa58b7951ba54b40aa1af49c02
Shuudy <ancieux.jerome059@gmail.com>
2024-09-29 00:57:51 +0200

    Optimize setTimes function for better performance and avoid use depreciated method substr

--- resources/js/app.js
+++ resources/js/app.js
@@ -147,14 +147,16 @@ window.addEventListener("load", () => {
     }
     
     track.connect(gainNode).connect(audioCtx.destination);
-    
+
     function setTimes() {
-        playerCurrentTime.textContent = new Date(audioElement.currentTime * 1000)
-            .toISOString()
-            .substr(14, 5)
-        playerDuration.textContent = new Date(audioElement.duration * 1000)
-            .toISOString()
-            .substr(14, 5)
+        const formatTime = (time) => {
+            const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+            const seconds = Math.floor(time % 60).toString().padStart(2, '0');
+            return `${minutes}:${seconds}`;
+        };
+
+        playerCurrentTime.textContent = formatTime(audioElement.currentTime);
+        playerDuration.textContent = formatTime(audioElement.duration);
     }
 
     function progressUpdate() {
